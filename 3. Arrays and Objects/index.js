4acdc8ae4be48c906e4c755c885eebdd9eb1848d
Shairah Nicole Manzon <shairahmanzon03@gmail.com>
2024-01-15 20:34:24 +0800

    Update index.js

--- 3. Arrays and Objects/index.js
+++ 3. Arrays and Objects/index.js
@@ -1,128 +1,163 @@
 /*
     Resources:
-    - MDN Docs: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Control_flow_and_error_handling
+    - JavaScript Tutorial on Objects: https://javascript.info/object
+    - freeCodeCamp on Objects: https://www.freecodecamp.org/news/objects-in-javascript-for-beginners/
+    - JavaScript Tutorial on Arrays: https://javascript.info/array
+    - freeCodeCamp on Arrays: https://www.freecodecamp.org/news/javascript-array-tutorial-array-methods-in-js/
 */
 
-const randomNumber = Math.floor((Math.random() * 100) + 1); // feel free to mock this value for testing
+let numbers = [
+    1, 89, 55, 63, 29, 19,
+    15, 77, 62, 68, 29, 84,
+    21, 26, 49
+];
 
-// TODO 2.1 Display "more than fifty" if randomNumber is more than fifty
-if (randomNumber > 50) {
-    console.log("more than fifty");
-} else {
-    console.log("fifty or less");
-}
+// Todo 3.1 Get the first, 5th, and last items in the numbers array.
+let numbers = [
+    1, 89, 55, 63, 29, 19,
+    15, 77, 62, 68, 29, 84,
+    21, 26, 49
+];
 
+const firstItem = numbers[0];
+const fifthItem = numbers[4];
+const lastItem = numbers[numbers.length - 1];
 
-// TODO 2.2 Display whether the random number is odd or even
-if (randomNumber % 2 === 0) {
-    console.log(`${randomNumber} is even`);
-} else {
-    console.log(`${randomNumber} is odd`);
-}
-
-console.log("Random Number:", randomNumber);
-
-
-// TODO 2.3 If the number if a multiple of 3, write "fizz".
-// if the number is a multiple of 5 display "buzz".
-// if the number is divisible by both 3 and 5, display "fizzbuzz". otherwise, display the number
-if (randomNumber % 3 === 0 && randomNumber % 5 === 0) {
-    console.log("fizzbuzz");
-} else if (randomNumber % 3 === 0) {
-    console.log("fizz");
-} else if (randomNumber % 5 === 0) {
-    console.log("buzz");
-} else {
-    console.log(randomNumber);
-}
-
-console.log("Random Number:", randomNumber);
+console.log("First Item:", firstItem);
+console.log("Fifth Item:", fifthItem);
+console.log("Last Item:", lastItem);
 
 
-// TODO 2.4 Use the ternary conditional operator to set toDisplay to "Even" if randomNumber is even and "Odd" if randomNumber is odd
-const toDisplay = randomNumber % 2 === 0 ? "Even" : "Odd";
+// Todo 3.2 calculate the min, max, and the average of the numbers array
+let numbers = [
+    1, 89, 55, 63, 29, 19,
+    15, 77, 62, 68, 29, 84,
+    21, 26, 49
+];
 
-console.log(`Random Number: ${randomNumber}`);
-console.log(`toDisplay: ${toDisplay}`);
+// Calculate min, max, and average of the numbers array
+const minNumber = Math.min(...numbers);
+const maxNumber = Math.max(...numbers);
+const sum = numbers.reduce((acc, num) => acc + num, 0);
+const average = sum / numbers.length;
 
-
-// Checkpoint 2.1 How do you use switch statements and when would you use them? Try researching the answer
-// Answer: The switch statement is used to test thge equality of a variable against several valuesspecified in the test cases.
-
-// TODO 2.5 Use a for loop to print the numbers 1 to N
-const N = 10;
-
-for (let i = 1; i <= N; i++) {
-    console.log(i);
-}
-
-const list = ["apple", "banana", "cherry", "date", "elderberry"];
-// TODO 2.6 Use a while loop to display all the values in the list
-let index = 0;
-
-while (index < list.length) {
-    console.log(list[index]);
-    index++;
-}
+console.log("Minimum Number:", minNumber);
+console.log("Maximum Number:", maxNumber);
+console.log("Average:", average.toFixed(2)); 
 
 
-// Checkpoint 2.3 How would you simulate a do-while loop in JavaScript
-// Simulating a do-while loop
-let condition;
-do {
-    console.log("This code will be executed at least once.");
+// Checkpoint 3.1 How would you compare lists in python vs arrays in JavaScript
+// Answer: 
+Python Lists: Can contain elements of different data types, and the type of the list can change dynamically.
+JavaScript Arrays: Can also contain elements of different data types, but JavaScript itself is dynamically typed, so the type of the array can change dynamically.
 
-    condition = false; 
-} while (condition);
+// Todo 3.3 Declare an object with information about IT114L (course code, name, units, number of students)
+const it114lCourse = {
+    courseCode: "IT114L",
+    courseName: "Introduction to Web Development",
+    units: 3,
+    numberOfStudents: 50 
 
-
-// TODO 2.7 Use a for of loop to display all the values in the list
-const list = ["apple", "banana", "cherry", "date", "elderberry"];
-
-for (const value of list) {
-    console.log(value);
-}
+console.log(it114lCourse);
 
 
-// TODO 2.8 Use a for in loop to display all the values in the list
-const list = ["apple", "banana", "cherry", "date", "elderberry"];
+// Todo 3.4 Add professor name as one of the fields of the object. Display the value of professor name.
+const it114lCourse = {
+    courseCode: "IT114L",
+    courseName: "Introduction to Web Development",
+    units: 3,
+    numberOfStudents: 50,
+    professorName: "Job Lipat"
+};
 
-for (const index in list) {
-    const value = list[index];
-    console.log(value);
-}
+
+// Display the value of professorName
+console.log("Job Lipat:", it114lCourse.professorName);
+
+// Todo 3.5 Declare and array of objects with information about the courses you are taking this term
+const coursesThisTerm = [
+    {
+        courseCode: "IT114L",
+        courseName: "Introduction to Web Development",
+        units: 3,
+        numberOfStudents: 50,
+        professorName: "Job Lipat"
+    },
+    {
+        courseCode: "CS102-1",
+        courseName: "Data Structures and Algorithms",
+        units: 2,
+        numberOfStudents: 40,
+        professorName: "Terrence Lim"
+    },
+    {
+        courseCode: "IT132L",
+        courseName: "Logic Design and Switching (Laboratory)",
+        units: 2,
+        numberOfStudents: 30,
+        professorName: "Leonnel De Mesa"
+    }
+    {
+        courseCode: IT133",
+        courseName: "Technopreneurship",
+        units: 3,
+        numberOfStudents: 41,
+        professorName: "Kristian Kikuchi"
+    }
+    {
+        courseCode: IT114",
+        courseName: "Web Systems and Technologies",
+        units: 2,
+        numberOfStudents: 42,
+        professorName: "Adomar Ilao"
+    }
+    
+];
 
 
-// TODO 2.9 Use the for each method of the list to display all its values 
-const list = ["apple", "banana", "cherry", "date", "elderberry"];
+// Display information about each course
+coursesThisTerm.forEach(course => {
+     console.log(`IT114: ${course.courseCode}`);
+     console.log(`Web Systems and Technologies: ${course.courseName}`);
+     console.log(`2: ${course.units}`);
+     console.log(`42: ${course.numberOfStudents}`);
+     console.log(`Adomar Ilao: ${course.professorName}`);
+     console.log("--------------");
 
-list.forEach(value => {
-    console.log(value);
 });
 
 
-// Checkpoint 2.2 When should you use for of, for in, or .forEach loops? Try researching the answer
+// Todo 3.5 Calculate the total number of units you are taking this term using the array of objects.
+const totalUnits = coursesThisTerm.reduce((sum, course) => sum + course.units, 0);
+
+console.log("Total Units this Term:", totalUnits);
+
+
+// Checkpoint 3.2 What would be the equivalent of objects in python? How would you compare them?
 // Answer: 
-for...of loop: Best suited for iterating over iterable objects like arrays, strings, or other iterable collections.
-for...in loop: Primarily used for iterating over the properties of an object. It iterates over all enumerable properties, including those in the prototype chain.
-forEach method: Specifically designed for arrays, and it allows you to iterate over each element in a more concise and expressive manner.
+JavaScript objects are often used as prototypes for creating instances using constructor functions or classes.
+Python dictionaries are part of the core language and are used widely for various purposes.
 
-// TODO 2.10 Use the try and catch block to catch division by zero errors in the code below.
-// In the finally block, simulate cleaning up resources by displaying "cleaning up resources"
-try {
-    const numerator = Math.floor((Math.random() * 100) + 1);
-    const denominator = Math.floor((Math.random() * 5)); // Mocked value for testing
+// Todo 3.6 Going back to the array of numbers, use the spread syntax to create a copy of the array with an additional number
+const originalNumbers = [1, 2, 3, 4, 5];
+const additionalNumber = 6;
 
-    if (denominator === 0) {
-        throw new Error("Division by zero error");
-    }
+const newArrayWithAdditionalNumber = [...originalNumbers, additionalNumber];
 
-    const quotient = numerator / denominator;
-    console.log("Quotient:", quotient);
-} catch (error) {
-    console.error("Error:", error.message);
-} finally {
-    console.log("Cleaning up resources");
-}
+console.log("Original Numbers:", originalNumbers);
+console.log("New Array with Additional Number:", newArrayWithAdditionalNumber);
 
 
+// Todo 3.7 Going back to your IT114L object, extract the course code and units using the spread operator
+const IT114L = {
+    courseCode: "IT114L",
+    courseName: "Introduction to Web Development",
+    units: 3,
+    numberOfStudents: 50,
+    professorName: "Job Lipat"
+};
+
+const { courseCode, units } = IT114L;
+
+console.log("Course Code:", courseCode);
+console.log("Units:", units);
