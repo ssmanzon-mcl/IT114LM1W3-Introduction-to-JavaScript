3347eccf8d8fdf49cee2060d2635d43405a8bd00
Shairah Nicole Manzon <shairahmanzon03@gmail.com>
2024-01-16 21:00:01 +0800

    Update index.js

--- 5. Map, Filter, and Reduce/index.js
+++ 5. Map, Filter, and Reduce/index.js
@@ -60,6 +60,6 @@ console.log("Concatenated String:", concatenatedString.trim());
 
 // Checkpoint 4.1 Summarize what map, filter, and reduce do
 // Answer: 
-map: Transforms each element of an array based on a provided callback function.
-filter: Creates a new array containing only the elements that pass a specified condition.
-reduce: Reduces the array to a single value by applying a function against an accumulator and each element.
+// map: Transforms each element of an array based on a provided callback function.
+// filter: Creates a new array containing only the elements that pass a specified condition.
+// reduce: Reduces the array to a single value by applying a function against an accumulator and each element.
